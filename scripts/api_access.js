f98417c159861750a50fff771e099bf497d0bb89
Joshua Barua <joshuabarua9@hotmail.com>
2023-06-14 13:06:50 +0200

    Initial Commit

---
+++ scripts/api_access.js
@@ -0,0 +1,52 @@
+const units = "metric";
+const timesteps = "1h";
+const baseUrl = "https://api.tomorrow.io/v4";
+const apikey = "";
+const request = new XMLHttpRequest();
+
+// Create a request variable and assign a new XMLHttpRequest object to it.
+const getCurrentPosition = () => {
+	return new Promise((resolve, reject) => {
+		navigator.geolocation.getCurrentPosition(resolve, reject);
+	});
+};
+
+const getPositionData = async () => {
+	try {
+		const position = await getCurrentPosition();
+		const latitude = position.coords.latitude;
+		const longitude = position.coords.longitude;
+		const userLocation = `${latitude},${longitude}`;
+
+		makeApiRequest(userLocation);
+	} catch (error) {
+		console.error(error);
+	}
+};
+
+const makeApiRequest = async (userLocation) => {
+	const apiUrl = `${baseUrl}/weather/forecast?location=${userLocation}&units=${units}&timesteps=${timesteps}&apikey=${apikey}`;
+	try {
+		const response = await fetch(apiUrl);
+		if (!response.ok) {
+			throw new Error(`API request failed with status: ${response.status}`);
+		}
+
+		const data = await response.json();
+		return data;
+	} catch (error) {
+		console.error("API request failed:", error);
+	}
+};
+
+const processApiData = (data) => {
+	// Process the received data as needed
+	console.log(data);
+
+	// Further processing or logic using the API response data
+	// Example: Accessing specific data properties
+	const temperature = data.data.timelines[0].intervals[0].values.temperature;
+	console.log("Temperature:", temperature);
+};
+
+getPositionData();
